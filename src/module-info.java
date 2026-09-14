ce57fd568b610c319b28bc1142282b92ec4745e1
wilsonDAM1 <wilson.toapanta@educa.madrid.org>
2026-09-14 16:26:30 +0200

    Commit Inicial

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Tienda_Practica {
+}
---
+++ src/tienda/Tienda.java
@@ -0,0 +1,10 @@
+package tienda;
+
+public class Tienda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
