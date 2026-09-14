053be0901561569a8a52631cce68791595a6aa78
wilsonDAM1 <wilson.toapanta@educa.madrid.org>
2026-09-14 16:51:30 +0200

    Subiendo mi rama

--- src/tienda/Tienda.java
+++ src/tienda/Tienda.java
@@ -3,8 +3,12 @@ package tienda;
 public class Tienda {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("...");
+		
+		
+		
+		
+		 //tengo mucha plata locoooooo
 	}
 
 }
